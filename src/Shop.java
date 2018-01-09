fa96a7b417eb12222a3f1a0cbad6545040ef6c0e
RumbleFrog <xxlosttechxx@gmail.com>
2018-01-09 10:49:34 -0500

    Shop Summary Section

--- src/Shop.java
+++ src/Shop.java
@@ -7,7 +7,7 @@ public class Shop
     {
         ArrayList<Item> cart = new ArrayList<>();
         String itemName;
-        double itemPrice;
+        double itemPrice, fSubTotal, fTotal, fTax;
         int quantity;
         String keepShopping = "y";
         Scanner scan = new Scanner(System.in);
@@ -27,12 +27,22 @@ public class Shop
             System.out.println("Item    \t\tPrice\t   Quantity\t   Item Total");
             System.out.println("***************************************************");
 
+            fSubTotal = 0;
+
             for (Item item : cart)
             {
                 System.out.println(String.format("%s\t\t        $%.2f      %d\t\t   $%.2f", item.getName(), item.getPrice(), item.getQuantity(), item.getPrice() * item.getQuantity()));
+                fSubTotal += item.getPrice();
             }
 
+            fTax = fSubTotal * 0.0635;
+            fTotal = fSubTotal + fTax;
 
+            System.out.println("***************************************************");
+            System.out.println(String.format("Subtotal\t\t\t\t      $%.2f", fSubTotal));
+            System.out.println(String.format("Tax\t\t\t\t\t      $%.2f", fTax));
+            System.out.println(String.format("Total\t\t\t\t\t      $%.2f", fTotal));
+            System.out.println("==============================================");
 
             System.out.print("Continue shopping (y/n)? ");
             keepShopping = scan.next();
